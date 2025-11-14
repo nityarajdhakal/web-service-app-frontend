c0d5734ce86f042f0d359f6958d75d50ca654a9c
nityarajdhakal <nityarajdhakal99@gmail.com>
2025-11-15 02:39:38 +0545

    for deployment

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,8 @@ import HamburgerMenu from "../components/HamburgerMenu";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 
-const API_BASE_URL = "https://web-service-app-wo6z.onrender.com/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 
 const LoginPage = () => {
   const navigate = useNavigate();
--- src/pages/TermsPage.jsx
+++ src/pages/TermsPage.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "./TermsPage.css";
 import HamburgerMenu from "../components/HamburgerMenu";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 
 const TermsPage = () => {
     const navigate = useNavigate();
